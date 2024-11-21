e772e3720879f369ba3e85b074ce194b6e352e4d
VanceGoblin <zvkubajak@gmail.com>
2024-11-21 16:38:19 -0600

    Created Login Logic in authAPI.tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,27 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try {
+    const res = await fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log("Error from user login:", error);
+    return Promise.reject("Could not fetch user info.");
+  }
 }
-
-
 
 export { login };
